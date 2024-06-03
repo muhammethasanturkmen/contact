d3bcfd74b842167d6441607d904404f17724ca22
muhammet hasan <muhammethasanturkmen@gmail.com>
2024-06-04 02:16:10 +0300

    devam edilecek son kayıt

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,13 @@
-let isim = document.querySelector(".inputname");
+let isim = document.querySelector(".input-name");
 let soy = document.querySelector(".input-surname");
 let yazİsim = document.querySelector(".first-name");
-let yazSoy = document.querySelector(".surname");
+let yazSoy = document.querySelector(".sur-name");
 let submit = document.querySelector(".button-one");
 
-function handlinkclick() {
+function handlinkclick(e) {
+  e.preventDefault();
   yazİsim.innerText = isim.value;
+  yazSoy.innerText = soy.value;
 }
 
 submit.addEventListener("click", handlinkclick);
